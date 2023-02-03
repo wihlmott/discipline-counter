2beb47fafaa782c41b712ff876b56360a7b07eaa
wihlmott <wit.valentine@gmail.com>
2023-02-03 16:58:44 +0200

    beforeBackend commit

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABaT5fzMFmyPSUkWLB1SIW3XHfBWZUDxY",
+  authDomain: "discipline-counter.firebaseapp.com",
+  projectId: "discipline-counter",
+  storageBucket: "discipline-counter.appspot.com",
+  messagingSenderId: "855659707264",
+  appId: "1:855659707264:web:dc0ef92077dc0a349c5f65"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
